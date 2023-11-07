11f97768b81424c3b7b0befc40493d47dcf253a2
Andrew Kim <andrewhkim3@gmail.com>
2023-11-07 15:39:57 -0600

    switch test file from txt to py

--- tests/deliverable_1_testcases.py
+++ tests/deliverable_1_testcases.py
@@ -15,11 +15,11 @@ arith_op3 *= arith_op4
 arith_op4 /= arith_op5
 
 array1 = [1, 2, 3, 4, 5]
-array2 = ['a', 'b', 'c']
+array2 = ["a", "b", "c"]
 array3 = [1.6, 2.7, 3.8, 4.9, 5.0]
 
 var1 = 10
-var2 = var1/2 + 5
+var2 = var1 / 2 + 5
 var3 = var2 % 2
 var4 = 1
 
